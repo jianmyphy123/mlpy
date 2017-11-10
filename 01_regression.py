1c55a16060930631cd19bb45606b6c2575e156f2
jianmyphy123 <jianmyphy123@tutanota.com>
2017-11-10 21:06:13 +1100

    regression features and labels

--- 01_regression.py
+++ 01_regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -8,5 +9,11 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+df.fillna(-99999, inplace=True)
 
+forecast_col = 'Adj. Close'
+
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
 print(df.head())
